136d7d773b182b43479c45ce6aed06c3e761fa65
siradriaan <sir.adss.23@gmail.com>
2020-10-23 07:49:58 +0700

    deploy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const routes = require('./routes/index.js')
 const errorHandler = require('./middlewares/errorHandler')
 
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
